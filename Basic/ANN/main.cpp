8178c6750b8a9d7e203b57e78778b87689f738be
Wenyu <0910692@mail.nankai.edu.cn>
2019-03-01 10:35:15 +0800

    test incremental training method

--- Basic/ANN/main.cpp
+++ Basic/ANN/main.cpp
@@ -1,13 +1,14 @@
 /*
 	Author: Wenyu
-	Date: 2/28/2019
-	Version: 1.2
+	Date: 03/01/2019
+	Version: 1.3
 	Env: Opencv 3.4 vc14, VS2015 Release x64, "gazeEstimate.h", "gazeEstimate.cpp"
 	Function:
 	v1.0: process gaze data and model an ANN from 12-D inputs to 2-D screen points
 		as a demo
 	v1.1: add mat release
 	v1.2: use namespace ge
+	v1.3: test incremental training method
 */
 
 #include <iostream>
@@ -19,7 +20,7 @@
 
 int main() {
 	// import data
-	const int amount = 5388;
+	const int amount = 6690;
 	std::ifstream f_data("data7.txt");
 	cv::Mat MD = cv::Mat_<float>(amount, 14);
 	for (int index = 0; index < amount; ++index) {
@@ -38,9 +39,14 @@ int main() {
 	cv::Mat trainInputs = MD(cv::Rect(0, 0, 12, nTrain));
 	cv::Mat trainOutputs = MD(cv::Rect(12, 0, 2, nTrain));
 
-	int nTest = amount - nTrain;
+	int nTest = amount - nTrain - 10;
 	cv::Mat testInputs = MD(cv::Rect(0, nTrain, 12, nTest));
 	cv::Mat testOutputs = MD(cv::Rect(12, nTrain, 2, nTest));
+
+	// split samples for incremental learning
+	int nIncTrain = amount - nTrain - nTest;
+	cv::Mat incTrainInputs = MD(cv::Rect(0, nTrain + nTest, 12, nIncTrain));
+	cv::Mat incTrainOutputs = MD(cv::Rect(12, nTrain + nTest, 2, nIncTrain));
 
 	// train n test
 	ge::GazeEst gE;
@@ -57,6 +63,14 @@ int main() {
 	std::cout << "testLoss = " << testLoss << "\ttime = " << gE.getTestTime() << std::endl;
 
 	//cout << pre << endl;
+	float incTrainLoss = 
+		gE.incTrain(incTrainInputs, 
+			incTrainOutputs, 
+			testInputs, 
+			testOutputs, 
+			true);
+	std::cout << "incTrainLoss = " << incTrainLoss << "\ttime = " << gE.getIncTrainTime() << std::endl;
+
 	
 	// save model
 	gE.save("latest_model_6e70.xml");
