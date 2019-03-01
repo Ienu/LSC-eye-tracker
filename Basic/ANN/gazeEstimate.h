2e2a7819cd2e85f0816581c453736be13868c41a
Wenyu <0910692@mail.nankai.edu.cn>
2019-03-01 10:33:47 +0800

    add incrementally training method

--- Basic/ANN/gazeEstimate.h
+++ Basic/ANN/gazeEstimate.h
@@ -1,7 +1,7 @@
 /*
 	Author: Wenyu
-	Date: 2/28/2019
-	Version: 1.5
+	Date: 03/01/2019
+	Version: 1.6
 	Env: Opencv 3.4 vc14, VS2015 Release x64
 	Function:
 	v1.0: process gaze data and model an ANN from 12-D inputs to 2-D screen points
@@ -12,6 +12,7 @@
 	loss
 	v1.4: change opt method to rprop
 	v1.5: improve code with namespace and add adaptive training stop
+	v1.6: add incrementally training method
 */
 
 #pragma once
@@ -34,7 +35,10 @@ namespace ge {
 		const float y_scale[2] = { 1920, 1080 };
 
 		double m_train_time;
+		double m_inc_train_time;
 		double m_pre_time;
+
+		unsigned int m_n_samples;
 
 	public:
 
@@ -53,7 +57,7 @@ namespace ge {
 		@param trainOutputs: sample labels for training, each row indicates one sample,	the
 			number of rows should equal to the training samples
 		@param stop_error: stop error for training, when the error is less than the stop error,
-			the process halts
+			the process halts, default value is -1, it will stop adaptively
 		@param testInputs: samples for testing, ecah row indicates one sample
 		@param testOuputs: sample labels for training, each row indicates one sample, the number
 			of rows should equal to the testing samples
@@ -61,7 +65,23 @@ namespace ge {
 		 */
 		float train(const cv::Mat& trainInputs,
 			const cv::Mat& trainOutputs,
-			float stop_error = -1,
+			float stopError = -1,
+			const cv::Mat& testInputs = cv::Mat(),
+			const cv::Mat& testOutputs = cv::Mat(),
+			bool verbose = false);
+
+		/** @incrementally train the model with specific params, output the training loss
+
+		@param trainInputs: samples for training, each row indicates one sample
+		@param trainOutputs: sample labels for training, each row indicates one sample,	the
+			number of rows should equal to the training samples
+		@param testInputs: samples for testing, ecah row indicates one sample
+		@param testOuputs: sample labels for training, each row indicates one sample, the number
+			of rows should equal to the testing samples
+		@param verbose: true for display training process
+		 */
+		float incTrain(const cv::Mat& trainInputs,
+			const cv::Mat& trainOutputs, 
 			const cv::Mat& testInputs = cv::Mat(),
 			const cv::Mat& testOutputs = cv::Mat(),
 			bool verbose = false);
@@ -69,11 +89,19 @@ namespace ge {
 		/** @get the training time */
 		double getTrainTime();
 
+		/** @get the incremental training time */
+		double getIncTrainTime();
+
+		/** @get the number of trained samples in total */
+		unsigned int getNumTrained();
+
 		/** @load pre-trained xml model file
 
 		@param fileName: xml file path
+		@param nSamples: previous training samples amount, for incremental train,
+			default value is -1, which makes the incTrain learn the new samples once
 		 */
-		void load(const char* fileName);
+		void load(const char* fileName, int nSamples = -1);
 
 		/** @save trained xml model file
 
